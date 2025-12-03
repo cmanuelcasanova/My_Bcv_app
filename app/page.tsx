dc25f4c33abcd4af872daec4e50f83fc87006dc7
manuel <manuel.casanova.ucv@gmail.com>
2025-12-02 22:30:48 -0400

    Varios 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,137 @@
+'use client'
+
 import Image from "next/image";
+import { useEffect , useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form"
+import { RiMoneyDollarBoxFill } from "react-icons/ri";
+
+export interface Api {
+  current: Current
+  previous: Previous
+  changePercentage: ChangePercentage
+}
+
+interface Current {
+  usd: number
+  eur: number
+  date: string
+}
+
+interface Previous {
+  usd: number
+  eur: number
+  date: string
+}
+
+interface ChangePercentage {
+  usd: number
+  eur: number
+}
+
+type FormData = {
+  cantidad: number;
+  
+};
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+
+   const [dataF, setDataF] = useState<Api | null>(null);
+   const [loading, setLoading] = useState(true);
+   const [error, setError] = useState(null);
+   const [calculo, setCalculo] = useState<number>(0);
+   const { register, handleSubmit, watch} = useForm<FormData>()
+   
+   const cantidad_divisas = watch('cantidad'); 
+
+   const onSubmit = handleSubmit(async (data) => {
+
+      
+      if(dataF) setCalculo(  Math.round((data.cantidad*dataF?.current.eur)*100) /100   )
+
+   
+
+   })
+
+    
+
+
+ useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await fetch('https://api.dolarvzla.com/public/exchange-rate');
+          if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          const result = await response.json();
+          setDataF(result);
+        } catch (error) {
+          console.log(error);
+        } finally {
+          setLoading(false);
+        }
+      };
+
+      fetchData();
+    }, []); 
+
+ useEffect(() => {
+
+   if(dataF) setCalculo(  Math.round((cantidad_divisas*dataF?.current.eur)*100) /100   )
+
+
+   }, [cantidad_divisas]); 
+    
+    
+
+
+
+    return (
+    <div className="flex min-h-screen items-center justify-center bg-linear-to-b from-blue-950 via-blue-900 to-black">
+     {dataF && 
+     
+      <div className="flex flex-col items-center justify-center">
+  
+        <Image 
+          src="/logobcv.png" 
+          alt="Picture of the author" 
+          width={200} 
+          height={200} 
+          loading="eager"
+          className="mb-30 rounded-2xl w-50"
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+        <span>Fecha Actualizacion: {dataF.current.date}</span>
+        <span>Valor EurBcv:  {Math.round(dataF.current.eur*100)/100} Bs. </span>
+   
+        
+            
+           
+
+
+          <form onSubmit={onSubmit}>
+          
+          <div className="flex flex-col gap-2 items-center justify- mt-10">
+          
+          <div className="flex flex-wrap items-center justify-center">
+          <input 
+            type="number" 
+            pattern="[0-9]*" 
+            className="bg-white w-50 font-bold text-black text-2xl text-center rounded-2xl"
+            required
+            {...register('cantidad')} />
+
+            <span> <RiMoneyDollarBoxFill size={40} /> </span>
+          </div>
+
+          <button type="submit">Calcular</button>
+          </div>
+        </form>
+
+         <div className="bg-white text-black font-bold mt-20 w-60 shadow rounded-2xl text-center text-3xl"> {calculo} Bs. </div>
+
+
+
+     </div>
+     }
     </div>
   );
 }
